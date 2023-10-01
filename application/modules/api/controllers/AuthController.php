f66b7f41733eb11e23cf7261fed194adad75834a
nureke <sabyrzhan.nurlan@gmail.com>
2023-10-01 13:01:57 +0600

    Signed-off-by: nureke <sabyrzhan.nurlan@gmail.com>

--- application/modules/api/controllers/AuthController.php
+++ application/modules/api/controllers/AuthController.php
@@ -82,7 +82,7 @@ class Api_AuthController extends Zend_Controller_Action {
             $encryptedValue = encryptCookie($originalValue, $key);
 
             if($this->debug){
-                setcookie("secureCookie", $encryptedValue, time() + (7 * 24 * 60 * 60), "/", ".itp.kz", false, false);
+                setcookie("secureCookie", $encryptedValue, time() + (7 * 24 * 60 * 60), "/", ".localhost", true, true);
             }else{
                 setcookie("secureCookie", $encryptedValue, time() + (7 * 24 * 60 * 60), "/", ".i-helath.kz", true, true);
             }
@@ -130,7 +130,7 @@ class Api_AuthController extends Zend_Controller_Action {
             // Шифрование нового refresh token и сохранение в cookie
             $encryptedNewRefreshToken = encryptCookie($newRefreshToken['refresh_token'], $key);
             if($this->debug){
-                setcookie("secureCookie", $encryptedNewRefreshToken, time() + (7 * 24 * 60 * 60), "/", ".itp.kz", false, false);
+                setcookie("secureCookie", $encryptedNewRefreshToken, time() + (7 * 24 * 60 * 60), "/", ".localhost", true, true);
             }else{
                 setcookie("secureCookie", $encryptedNewRefreshToken, time() + (7 * 24 * 60 * 60), "/", ".i-helath.kz", true, true);
             }
