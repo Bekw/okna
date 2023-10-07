6d028537c83f889bb644e9494b361ea188dc041f
nureke <sabyrzhan.nurlan@gmail.com>
2023-10-07 19:19:45 +0600

    Signed-off-by: nureke <sabyrzhan.nurlan@gmail.com>

--- application/modules/api/controllers/ClientController.php
+++ application/modules/api/controllers/ClientController.php
@@ -28,6 +28,17 @@ class Api_ClientController extends Api_ParentController
         }
         $this->sendResponse($row['value']);
     }
+
+    public function clientProfileAction(){
+        $ob = new Api_Model_DbTable_Client();
+        $client_id = $this->_getParam('client_id', 0);
+        $row = $ob->client_profile__get($client_id);
+        if(!$row['status']){
+            $this->sendResponse(null, self::HTTP_INTERNAL_SERVER_ERROR, $row['error']);
+            return;
+        }
+        $this->sendResponse($row['value']);
+    }
 }
 
 
--- application/modules/api/models/DbTable/Client.php
+++ application/modules/api/models/DbTable/Client.php
@@ -5,4 +5,9 @@ class Api_Model_DbTable_Client extends Application_Model_DbTable_Parent {
         $result = $this->readSP(__FUNCTION__, "public.client_order_hist__read('cur', :client_id)", $p);
         return $result;
     }
+    public function client_profile__get($client_id){
+        $p['client_id'] = $client_id;
+        $result = $this->getSP(__FUNCTION__, "public.client_profile__get('cur', :client_id)", $p);
+        return $result;
+    }
 }
