10fd976095df71bb69c523505cf7bc9351a6c592
nureke <sabyrzhan.nurlan@gmail.com>
2023-10-14 21:02:24 +0600

    Signed-off-by: nureke <sabyrzhan.nurlan@gmail.com>

--- application/modules/api/models/DbTable/Order.php
+++ application/modules/api/models/DbTable/Order.php
@@ -2,7 +2,7 @@
 class Api_Model_DbTable_Order extends Application_Model_DbTable_Parent {
     public function product__add_to_cart($order_id, $client_id, $product_id, $stock_id){
         $p['order_id'] = $order_id;
-        $p['client_id'] = $client_id;
+        $p['client_id'] = zeroToNull($client_id);
         $p['product_id'] = $product_id;
         $p['stock_id'] = $stock_id;
         $result = $this->execSP(__FUNCTION__, "public.product__add_to_cart(:order_id, :client_id, :product_id, :stock_id) id", $p, 'id');
