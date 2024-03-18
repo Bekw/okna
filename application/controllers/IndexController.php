9ec386aff5cfaa726fe85c4ec8c87371f52460bb
Bekw <bekwitmail@gmail.com>
2024-03-18 12:38:03 +0500

    Signed-off-by: Bekw <bekwitmail@gmail.com>

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -43,18 +43,8 @@ class IndexController extends ParentController{
     }
 
     public function indexAction(){
-//        $this->_redirect('/system/login/');
-        $this->_helper->layout->disableLayout();
-        $ob = new Application_Model_DbTable_Remont();
-
-        $mode = $this->_getParam('mode', '');
-        if ($mode == 'upd'){
-            $this->_helper->AjaxContext()->addActionContext('index', 'json')->initContext('json');
-            $params = $this->getAllParams();
-            $this->view->result = $ob->create_landing_request($params);
-            return;
-        }
-        $this->view->row = $ob->package_read_fs()['value'];
+        $this->_redirect('/system/login/');
+     
     }
 }
 
